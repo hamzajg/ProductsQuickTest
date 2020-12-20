0662c861c7753e1de07ff49e5b7d3cce10a18add
Hamza JGUERIM <hamzajg@gmail.com>
2020-12-20 17:55:42 +0100

    Config Api Gateway..

--- src/ApiGateway/src/main/java/com/hamzajg/quickstart/apigateway/ApiGatewayApplication.java
+++ src/ApiGateway/src/main/java/com/hamzajg/quickstart/apigateway/ApiGatewayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
